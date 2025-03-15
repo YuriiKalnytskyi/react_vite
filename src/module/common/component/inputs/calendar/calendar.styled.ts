518b626f3e4f904036dedbe0dfd17e21829dc792
YuriiKalnytskyi <yra6920@gmail.com>
2025-03-15 11:52:42 +0200

    fix calendar

--- src/module/common/component/inputs/calendar/calendar.styled.ts
+++ src/module/common/component/inputs/calendar/calendar.styled.ts
@@ -1,24 +1,18 @@
 import { DayPicker } from 'react-day-picker';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import {  INDEX, MEDIA } from '@/theme';
+import { PopupLayout } from '@/module/common/layout';
 import { IMargin } from '@/module/common/types';
+import { FONTS, INDEX, MEDIA } from '@/theme';
 
-export const Wrapper = styled.div<IMargin & { width?: string }>`
-    position: relative;
-
-    width: ${({ width }) => width ?? '100%'};
-
-    .endIcon {
-        transform: translate(0%, -50%) rotate(45deg);
+const dayPicker = css`
+    .rdp-multiple_months {
+        margin-top: 0;
+        border-color: ${({ theme }) => theme.COLORS.primary};
     }
 
-    .rdp-day_selected,
-    .rdp-day_selected:focus-visible,
-    .rdp-day_selected:hover {
-        background-color: ${ ({theme})=>  theme.COLORS.rgba(theme.COLORS.primary, 0.2)};
-        border-radius: 0;
-        color: #2c2c2c;
+    .endIcon {
+        background: #989aa0;
     }
 
     .rdp-button_reset.rdp-button.rdp-day.rdp-day_selected.rdp-day_range_end.rdp-day_range_start {
@@ -31,6 +25,7 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
         font-style: normal;
         font-weight: 500;
         line-height: 20px;
+        font-family: ${FONTS.FAMILIES.roboto};
     }
 
     .rdp-nav_icon {
@@ -41,13 +36,53 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
         color: #4d5560;
     }
 
+    .rdp-day_selected,
+    .rdp-day_selected:focus-visible,
+    .rdp-day_selected:hover {
+        background-color: ${({ theme }) => theme.COLORS.rgba(theme.COLORS.black, 0.2)};
+        border-radius: 0;
+        color: #2c2c2c;
+    }
+
+    .rdp-day {
+        font-weight: 400;
+        font-size: 0.875rem;
+    }
+
+    .rdp-day_today {
+        font-weight: 500;
+    }
+
+    .rdp-day_today:not(.rdp-day_outside, .rdp-day_range_middle) {
+        color: ${({ theme }) => theme.COLORS.white};
+        background-color: ${({ theme }) => theme.COLORS.black};
+        border-radius: 0.25rem;
+    }
+
+    .rdp-day_range_start,
+    .rdp-day_range_end {
+        color: ${({ theme }) => theme.COLORS.white};
+        background-color: ${({ theme }) => theme.COLORS.primary};
+    }
+
+    .rdp-day_range_start {
+        border-radius: 0.5rem 0.5rem 0.5rem;
+    }
+
+    .rdp-day_range_end {
+        border-radius: 0.5rem 0.5rem 0.5rem;
+    }
+
+    .rdp-day:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+
     .rdp-button_reset.rdp-button.rdp-nav_button.rdp-nav_button_previous:hover,
     .rdp-button_reset.rdp-button.rdp-nav_button.rdp-nav_button_next:hover {
         background: none;
     }
 
     .rdp-month {
-        /*width: 21.4375rem;*/
         margin: 0;
         font-size: 14px;
         font-style: normal;
@@ -56,7 +91,6 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
     }
 
     .rdp-month:first-child {
-        /*border-right: 1px solid #dce2ea;*/
         padding-right: 1rem;
     }
 
@@ -70,7 +104,7 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
     }
 
     .rdp-head_cell {
-        color: #8aaed8;
+        color: ${({ theme }) => theme.COLORS.black};
         font-size: 14px;
         font-style: normal;
         font-weight: 400;
@@ -80,18 +114,12 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
 
     .rdp-head_cell:last-child,
     .rdp-head_cell:nth-last-child(2) {
-        color: #f93232;
+        color: ${({ theme }) => theme.COLORS.black};
     }
 
     .rdp-cell {
         color: #2c2c2c;
         font-size: 0.875rem;
-    }
-
-    .rdp-day_today:not(.rdp-day_outside) {
-        color: ${ ({theme})=>  theme.COLORS.white};
-        background-color: ${ ({theme})=>  theme.COLORS.primary};
-        border-radius: 0.125rem;
     }
 
     .rdp-button:not([disabled]) {
@@ -101,32 +129,52 @@ export const Wrapper = styled.div<IMargin & { width?: string }>`
     }
 
     .rdp-button:hover:not([disabled]):not(.rdp-day_selected) {
-        color: ${ ({theme})=>  theme.COLORS.white};
-        background-color: ${ ({theme})=>  theme.COLORS.primary};
-        border-radius: 0;
+        color: ${({ theme }) => theme.COLORS.white};
+        background-color: ${({ theme }) => theme.COLORS.black};
+        border-radius: 0.25rem;
     }
 
+    .rdp {
+        margin: 0 1rem 0 0 !important;
+    }
+`;
 
+export const Wrapper = styled.div<IMargin & { width?: string }>`
+    position: relative;
+
+    width: ${({ width }) => width ?? '100%'};
+
+    ${dayPicker};
 `;
 
 export const Calendar = styled(DayPicker)`
-    cursor: pointer;
-    
+  cursor: pointer;
+  background: ${({ theme }) => theme.COLORS.white};
+  border-radius: 0.25rem;
 
-    background: ${ ({theme})=>  theme.COLORS.white};
-    border: 1px solid ${ ({theme})=>  theme.COLORS.rgba(theme.COLORS.black, 0.4)};
-    border-radius: 0.25rem;
+  border: 1px solid ${({ theme }) => theme.COLORS.white};
+  background: ${({ theme }) => theme.COLORS.white};
 
+  box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.1), 0px 2px 4px -2px rgba(0, 0, 0, 0.1);
 
-    @media screen and (min-width: ${MEDIA.tablet} ) {
-        position: absolute;
-        top: 2.5rem;
-        right: -1rem;
-        padding: 1rem;
-        z-index: ${INDEX.absolute};
-    }
+  @media screen and (min-width: ${MEDIA.tablet}) {
+    position: absolute;
+    top: 2.5rem;
+    right: -1rem;
+    padding: 1rem 0;
+    z-index: ${INDEX.absolute};
+  }
 
-    @media screen and (max-width: ${MEDIA.tablet_s}) {
-        margin: 0 auto;
-    }
+  @media screen and (max-width: ${MEDIA.tablet}) {
+    margin: 0 auto;
+  }
+`;
+
+export const PopupLayoutStyle = styled(PopupLayout)`
+  ${dayPicker};
+  align-items: center;
+
+  @media screen and (max-width: ${MEDIA.mobile_m}) {
+    padding: 3.5rem 0;
+  }
 `;
--- src/module/common/component/inputs/calendar/calendar.tsx
+++ src/module/common/component/inputs/calendar/calendar.tsx
@@ -6,16 +6,14 @@ import 'react-day-picker/dist/style.css';
 import calendarIcon from '@/assets/icons/default/calendar.svg';
 import closeIcon from '@/assets/icons/default/close-icon.svg';
 import { useClickOutside, useIsMobile } from '@/module/common/hooks';
+import { IInputDefault } from '@/module/common/types';
+import { MEDIA } from '@/theme';
 import { dateTransform, functionStub } from '@/utils';
 
 import { Input } from '../index';
 import * as Styled from './calendar.styled';
-import { IInputDefault } from '@/module/common/types';
-import { PopupLayout } from '@/module/common/layout';
-import { MEDIA } from '@/theme';
 
 export type DateSelection = DateRange | Date[] | Date | undefined;
-
 
 export interface ICalendarProps extends IInputDefault {
   readOnly?: boolean;
@@ -28,14 +26,14 @@ export interface ICalendarProps extends IInputDefault {
     setFieldValue: (name: string, value: DateSelection) => void;
   };
   disabledDay?: Date;
-  mode?: DaySelectionMode | undefined;
+  mode?: DaySelectionMode;
   isFlexLabel?: boolean;
   isSelectYearOrMounts?: { fromYear?: number; toYear?: number };
 }
 
 const CalendarFormatUtil = <T extends DateSelection>(
-  data: T,
-  mode: DaySelectionMode | undefined
+    data: T,
+    mode: DaySelectionMode | undefined
 ): string => {
   let filterDate = '';
 
@@ -46,8 +44,8 @@ const CalendarFormatUtil = <T extends DateSelection>(
     filterDate = `${startDate} - ${endDate.includes('NaN') ? '' : endDate}`;
   }
 
-  if (data && mode === 'single' && data instanceof Date) {
-    filterDate = dateTransform(data, true);
+  if (data && mode === 'single') {
+    filterDate = dateTransform(data as Date, true);
   }
 
   if (data && mode === 'multiple' && Array.isArray(data)) {
@@ -56,7 +54,6 @@ const CalendarFormatUtil = <T extends DateSelection>(
 
   return filterDate;
 };
-
 
 export const Calendar = ({
                            name,
@@ -72,26 +69,30 @@ export const Calendar = ({
                            visibleOfMonths,
                            ...props
                          }: ICalendarProps) => {
-  const { setFieldValue, value } = (() => {
+  const { setFieldValue, value, error, touched } = (() => {
     if (noFormikValue) {
       return {
         value: noFormikValue.value,
-        setFieldValue: noFormikValue.setFieldValue
+        setFieldValue: noFormikValue.setFieldValue,
+        error: '',
+        touched: false
       };
     } else {
       // eslint-disable-next-line react-hooks/rules-of-hooks
-      const { setFieldValue, values } = useFormikContext();
+      const { setFieldValue, values, errors, touched } = useFormikContext();
       return {
         value: getIn(values, name),
-        setFieldValue
+        setFieldValue,
+        error: getIn(errors, name),
+        touched: getIn(touched, name)
       };
     }
   })();
 
   const [isCalendarOpened, setIsCalendarOpened] = useState<boolean>(false);
 
-  const handleButtonClick = () => {
-    setIsCalendarOpened(!isCalendarOpened);
+  const handleButtonClick = (flag: boolean) => {
+    setIsCalendarOpened(flag);
   };
 
   const onSelect = (data: DateSelection | undefined) => {
@@ -103,88 +104,87 @@ export const Calendar = ({
     } else {
       setFieldValue(name, data);
     }
+    mode === 'single' && handleButtonClick(false);
   };
 
   const selected: DateSelection =
-    (mode === 'range' && (value as DateRange)) ||
-    (mode === 'multiple' && (value as Date[])) ||
-    (mode === 'single' && (value as Date)) ||
-    undefined;
+      (mode === 'range' && (value as DateRange)) ||
+      (mode === 'multiple' && (value as Date[])) ||
+      (mode === 'single' && (value as Date)) ||
+      undefined;
 
   const isMobile = useIsMobile();
-  const isTabletS = useIsMobile(MEDIA.tablet_s);
+  const isTabletS = useIsMobile(MEDIA.tablet);
 
   const { ref } = useClickOutside(() => {
-    if (isCalendarOpened && !isMobile) {
-      handleButtonClick();
+    if (!isMobile) {
+      handleButtonClick(false);
     }
   });
-
   const CalendarCommon = () => (
-    <Styled.Calendar
-      mode={mode as any}
-      onSelect={onSelect}
-      selected={selected as DateSelection}
-      numberOfMonths={isTabletS ? 1 : isMobile && visibleOfMonths === 1 ? 2 : visibleOfMonths ?? 2}
-      {...(disabledDay ? { disabled: { after: disabledDay } } : {})}
-      weekStartsOn={1}
-      {...(isSelectYearOrMounts
-        ? {
-          captionLayout: 'dropdown-buttons',
-          fromYear: isSelectYearOrMounts.fromYear ?? 2022,
-          toYear: isSelectYearOrMounts.toYear ?? new Date().getFullYear()
-        }
-        : { captionLayout: 'dropdown' })}
-    />
+      <Styled.Calendar
+          mode={mode as any}
+          onSelect={onSelect}
+          selected={selected as DateSelection}
+          numberOfMonths={isTabletS ? 1 : isMobile && visibleOfMonths === 1 ? 2 : visibleOfMonths ?? 2}
+          {...(disabledDay ? { disabled: { after: disabledDay } } : {})}
+          weekStartsOn={1}
+          {...(isSelectYearOrMounts
+              ? {
+                captionLayout: 'dropdown-buttons',
+                fromYear: isSelectYearOrMounts.fromYear ?? 2022,
+                toYear: isSelectYearOrMounts.toYear ?? new Date().getFullYear()
+              }
+              : { captionLayout: 'dropdown' })}
+      />
   );
 
-
   return (
-    <Styled.Wrapper ref={ref as RefObject<HTMLDivElement>} width={width} {...props} className="calendarContainer">
-      <Input
-        noFormikValue={{
-          value: CalendarFormatUtil(selected, mode),
-          setFieldValue: functionStub
-        }}
-        name={name}
-        label={label}
-        placeholder={placeholder}
-        width="100%"
-        height={height}
-        onClick={handleButtonClick}
-        startIcon={{ icon: calendarIcon }}
-        {...(value && {
-          endIcon: {
-            icon: closeIcon,
-            height: '1rem',
-            onClick: () => {
-              onSelect(undefined);
-            },
-            cursor: 'pointer'
-          }
-        })}
-        isDontChange
-        readOnly={readOnly}
-      />
+      <Styled.Wrapper
+          ref={ref as RefObject<HTMLDivElement>}
+          width={width}
+          {...props}
+          className='calendarContainer'
+      >
+        <Input
+            noFormikValue={{
+              value: CalendarFormatUtil(selected, mode),
+              setFieldValue: functionStub,
+              error,
+              touched
+            }}
+            name={name}
+            label={label}
+            placeholder={placeholder}
+            width='100%'
+            height={height}
+            onClick={handleButtonClick.bind(this, !isCalendarOpened)}
+            startIcon={{ icon: calendarIcon, background: '#989AA0' }}
+            {...(value && {
+              endIcon: {
+                icon: closeIcon,
+                height: '0.625rem',
+                onClick: () => {
+                  onSelect(undefined);
+                },
+                cursor: 'pointer'
+              }
+            })}
+            isDontChange
+            readOnly={readOnly}
+        />
 
-      {isCalendarOpened && !isMobile && (
-        <CalendarCommon />
-      )}
+        {isCalendarOpened && !isMobile && <CalendarCommon />}
 
-      {/*{isCalendarOpened && isMobile && (*/}
-      {/*<Drawer*/}
-      {/*  onClose={handleButtonClick}*/}
-      {/*  open={isCalendarOpened && isMobile}*/}
-      {/*  slidePosition="bottom"*/}
-      {/*  contentPosition="bottom"*/}
-      {/*>*/}
-        <PopupLayout height="50%" contentPosition="bottom" slidePosition='bottom' onClose={handleButtonClick} open={isCalendarOpened && isMobile}>
+        <Styled.PopupLayoutStyle
+            height='50%'
+            contentPosition='bottom'
+            slidePosition='bottom'
+            onClose={() => handleButtonClick(false)}
+            open={isCalendarOpened && isMobile}
+        >
           <CalendarCommon />
-        </PopupLayout>
-      {/*</Drawer>*/}
-      {/*)*/}
-      {/*}*/}
-
-    </Styled.Wrapper>
+        </Styled.PopupLayoutStyle>
+      </Styled.Wrapper>
   );
 };
